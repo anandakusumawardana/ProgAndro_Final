e25e3b9862b9dd7039e87381cd56cdbcb8bd4743
Ananda Kusumawardana <54304938+anandakusumawardana@users.noreply.github.com>
2019-09-07 21:34:02 +0700

    Added sign up,about class fix the homepage warnings Added signup.xml

--- app/src/main/java/com/example/progandro_final/SignUp.java
+++ app/src/main/java/com/example/progandro_final/SignUp.java
@@ -1,13 +1,19 @@
 package com.example.progandro_final;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class SignUp extends AppCompatActivity {
+    private Button daftar;
+    private EditText emailUser,pwd,pwd2;
 
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.signup);
+
+
     }
 }
