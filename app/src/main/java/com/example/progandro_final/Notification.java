0a242cebe8cde13714a2c17cb2dd35df88ac50a8
Ananda Kusumawardana <54304938+anandakusumawardana@users.noreply.github.com>
2019-09-22 16:41:49 +0700

    Add Broadcast Receiver and Notification

---
+++ app/src/main/java/com/example/progandro_final/Notification.java
@@ -0,0 +1,36 @@
+package com.example.progandro_final;
+
+import android.app.Application;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.os.Build;
+
+public class Notification extends Application {
+    public static final String onWiFi = "WiFi is On";
+    public static final String offWiFi = "WiFi is Off";
+    @Override
+    public void onCreate(){
+        super.onCreate();
+        createNotification();
+    }
+
+    private void createNotification() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+            NotificationChannel on = new NotificationChannel(
+                    onWiFi,
+                    "WiFi is On",
+                    NotificationManager.IMPORTANCE_HIGH);
+            on.setDescription("WiFi is On");
+
+            NotificationChannel off = new NotificationChannel(
+                    offWiFi,
+                    "WiFi is Off",
+                    NotificationManager.IMPORTANCE_LOW);
+            off.setDescription("WiFi is Off");
+
+            NotificationManager manager = getSystemService(NotificationManager.class);
+            manager.createNotificationChannel(on);
+            manager.createNotificationChannel(off);
+        }
+    }
+}
--- app/src/main/java/com/example/progandro_final/SimpleFragment.java
+++ app/src/main/java/com/example/progandro_final/SimpleFragment.java
@@ -6,28 +6,29 @@ import android.content.Intent;
 import android.content.IntentFilter;
 import android.net.wifi.WifiManager;
 import android.os.Bundle;
-
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.tabs.TabLayout;
-
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 import androidx.viewpager.widget.ViewPager;
 import androidx.appcompat.app.AppCompatActivity;
-
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
 import com.example.progandro_final.ui.main.SectionsPagerAdapter;
 
+import static com.example.progandro_final.Notification.offWiFi;
+import static com.example.progandro_final.Notification.onWiFi;
+
 public class SimpleFragment extends AppCompatActivity {
     private Button btn;
+    private NotificationManagerCompat notificationManagerCompat;
+    private View view;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_simple_fragment);
+        notificationManagerCompat = NotificationManagerCompat.from(this);
         SectionsPagerAdapter sectionsPagerAdapter = new SectionsPagerAdapter(this, getSupportFragmentManager());
         ViewPager viewPager = findViewById(R.id.view_pager);
         viewPager.setAdapter(sectionsPagerAdapter);
@@ -47,13 +48,16 @@ public class SimpleFragment extends AppCompatActivity {
         public void onReceive(Context context, Intent intent) {
             int wifiState = intent.getIntExtra(WifiManager.EXTRA_WIFI_STATE,WifiManager.WIFI_STATE_UNKNOWN);
             if (wifiState == WifiManager.WIFI_STATE_ENABLED){
+                wifiOn(view);
                 Toast.makeText(getApplicationContext(),"WiFi is On",Toast.LENGTH_SHORT).show();
             }
             else if (wifiState == WifiManager.WIFI_STATE_DISABLED){
+                wifiOff(view);
                 Toast.makeText(getApplicationContext(),"WiFi is Off",Toast.LENGTH_SHORT).show();
             }
         }
     };
+
     protected void onStart(){
         super.onStart();
         registerReceiver(broadcastReceiver, new IntentFilter(WifiManager.WIFI_STATE_CHANGED_ACTION));
@@ -61,5 +65,31 @@ public class SimpleFragment extends AppCompatActivity {
     protected void onStop(){
         super.onStop();
         unregisterReceiver(broadcastReceiver);
+    }
+    public void wifiOn(View view){
+        String title = "Apps";
+        String message = "WiFi is On";
+        android.app.Notification notification = new NotificationCompat.Builder(this,onWiFi)
+                .setSmallIcon(R.drawable.ic_wifi_black)
+                .setPriority(NotificationCompat.PRIORITY_HIGH)
+                .setCategory(NotificationCompat.CATEGORY_MESSAGE)
+                .setContentTitle(title)
+                .setContentText(message)
+                .build();
+
+        notificationManagerCompat.notify(1,notification);
+    }
+
+    public void wifiOff(View view){
+        String title = "Apps";
+        String message = "WiFi is Off";
+        android.app.Notification notification = new NotificationCompat.Builder(this,offWiFi)
+                .setSmallIcon(R.drawable.ic_wifi_black)
+                .setPriority(NotificationCompat.PRIORITY_LOW)
+                .setContentTitle(title)
+                .setContentText(message)
+                .build();
+
+        notificationManagerCompat.notify(2,notification);
     }
 }
